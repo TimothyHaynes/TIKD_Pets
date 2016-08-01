2dcca78a60b094e9098ff110c3f6792857b79012
Timothy Haynes <tim15811@gmail.com>
2016-08-01 10:01:51 -0400

    changes to our controller

--- js/animalCtrl.js
+++ js/animalCtrl.js
@@ -1,26 +1,44 @@
-var app= angular.module("badge-app");
+var app = angular.module("badgeApp");
 
-app.controller("ajaxCtrl1", function($http,$scope){
-$http ( {
-	method: 'GET',
-	url: 'https://teamtreehouse.com/katiegolicz.json'
-}).then (function successCallback(response) {
-	$scope.badges=response.data.badges;
-	console.log($scope.badges);
-},
-function errorCallback(response) {
-	console.log(response);
-});
-});
-
-app.controller("ajaxCtrl2", function($http, $scope) {
-	$http ({
-		method: 'GET',
-		url: 'https://teamtreehouse.com/davebrunetti.json'
-	}).then (function successCallback(response) {
-		$scope.badges=response.data.badges;
-	},
-	function errorCallback(response) {
-	console.log(response);
-});
+app.controller("ajaxCtrl1", function($http, $scope) {
+    $http({
+        method: 'GET',
+        url: 'https://teamtreehouse.com/katiegolicz.json'
+    }).then(function successCallback(response) {
+            $scope.badges = response.data.badges;
+            console.log($scope.badges);
+        },
+        function errorCallback(response) {
+            console.log(response);
+        });
+    $http({
+        method: 'GET',
+        url: 'https://teamtreehouse.com/davebrunetti.json'
+    }).then(function successCallback(response) {
+            $scope.badges = response.data.badges;
+            console.log($scope.badges);
+        },
+        function errorCallback(response) {
+            console.log(response);
+        });
+    $http({
+        method: 'GET',
+        url: 'https://teamtreehouse.com/ithicawilliams.json'
+    }).then(function successCallback(response) {
+            $scope.badges = response.data.badges;
+            console.log($scope.badges);
+        },
+        function errorCallback(response) {
+            console.log(response);
+        });
+    $http({
+        method: 'GET',
+        url: 'https://teamtreehouse.com/timothyhaynes2.json'
+    }).then(function successCallback(response) {
+            $scope.badges = response.data.badges;
+            console.log($scope.badges);
+        },
+        function errorCallback(response) {
+            console.log(response);
+        });
 });
